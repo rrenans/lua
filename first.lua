708115b63e9148792b3aff5eb7f2ff00271ccb51
Renan Silva <renansantos.silva04@outlook.com>
2022-11-10 09:31:38 -0300

    Meu primeiro código lua, este código é para entender o splash na prática

---
+++ first.lua
@@ -0,0 +1,33 @@
+function main(splash, args)
+    -- splash:set_user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+    --[[
+    headers = {
+      ['User-Agent'] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+    }
+    --]]
+    splash:on_request(function(request)
+        request:set_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36')
+    end)
+    splash:set_custom_headers(headers)
+    url = args.url
+      assert(splash:go(url))
+    assert(splash:wait(1))
+    
+    input_box = assert(splash:select("#search_form_input_homepage"))
+    input_box:focus()
+    input_box:send_text("my user agent")
+    assert(splash:wait(0.5))
+    --[[
+    btn = assert(splash:select("#search_button_homepage"))
+    btn:mouse_click()
+    --]]
+    
+    input_box:send_keys("<Enter>")
+    assert(splash:wait(5))
+    
+    splash:set_viewport_full()
+    return {
+      image = splash:png(),
+      html = splash:html()
+    }
+  end
